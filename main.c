96f58c8fc94fba6e9cf722aa16dba9d00406d389
Intars Ilgsalietis <ilgsalietisintars@gmail.com>
2025-01-16 19:38:49 +0000

    main: add newline, because scanf is broken
    Signed-off-by: Intars Ilgsalietis <ilgsalietisintars@gmail.com>

--- main.c
+++ main.c
@@ -23,6 +23,7 @@ int main() {
     printf("Vai jus velaties redzet esoso studentu datubazi(1/0)?\n");
     scanf("%d", &yn);
     if(yn==1){
+	printf("Lasa  %s\n", file);
         data = data_read(file, &c);
 	    if(!data)
 		printf("Kluda lasot datubazi: %m\n");
@@ -31,7 +32,7 @@ int main() {
 	return 0;
     }
 
-    printf("Ievadiet studentu skaitu: ");
+    printf("Ievadiet studentu skaitu:\n");
     scanf("%d",&c);
 
     s = (struct student *)malloc(sizeof(struct student)*c);
@@ -39,8 +40,8 @@ int main() {
     data_input(c,s);
     data_output(c,s);
 
-    printf("Vai Jus velaties saglabat datus(1/0)?");
-     err = scanf("%d",&yn);
+    printf("Vai Jus velaties saglabat datus(1/0)?\n");
+     err = scanf(" %d",&yn);
     if (yn==1){
 	printf("Dati tiek saglabati %s...\n", file);
 	data_save(c,s,file);
